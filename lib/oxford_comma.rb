0f57a293bf8b6241bd7d3d345f22a0e2b9edfe91
Faraz Hanif <hfaraz745@gmail.com>
2018-04-18 03:45:26 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length < 2
+    array.join
+  elsif array.length == 2
+    array.insert(1, "and").join(" ")
+  else
+    last = array.pop
+    string = array.join(", ") + ", and" + last  
+  end
 end
